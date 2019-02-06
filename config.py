268e9218521ac1382b962bf601c80182cf3a1e6c
Tim Beccue <timothybeccue@gmail.com>
2019-02-05 16:02:12 -0800

    app->application in application and config files

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ class Config(object):
     #    'users': 'sqlite:///'+os.path.join(basedir, 'databases/', users_name)
     #}
     SQLALCHEMY_BINDS = {
-        'things_in_space': 'sqlite:///'+os.path.join(basedir, 'databases/', skyobjects_name),
+        'things_in_space': 'mysql+pymysql://testdb11:testdb11@testdb1.cyuke35resta.us-west-1.rds.amazonaws.com:3306/things_in_space',
         'users': 'mysql+pymysql://testdb11:testdb11@testdb1.cyuke35resta.us-west-1.rds.amazonaws.com:3306/users' 
     }
 
